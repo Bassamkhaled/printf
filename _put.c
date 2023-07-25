8210d27491ee5ff2e081679fc4f612b305c0ff9e
Bassam <bassamk306@gmail.com>
2023-07-25 16:02:47 -0700

    printf project 100%

--- _put.c
+++ _put.c
@@ -19,15 +19,16 @@ int _puts(char *str)
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately
+ * Return: On success to print
+ *
+ * On error, -1 is returned, and errno is set appropriately.
 */
 int _putchar(int c)
 {
 	static int i;
-	static char buf[OUTPUT_BUF_SIZE]
+	static char buf[OUTPUT_BUF_SIZE];
 
-	if (c == Buf_FLUSH || i >= OUTPUT_BUF_SIZE)
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
 	{
 		write(1, buf, i);
 		i = 0;
